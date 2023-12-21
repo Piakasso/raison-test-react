0baa4f89cc0f7105bfc0207bb403b70f89f3db2f
Vitalii <number1pyak@gmail.com>
2023-12-21 22:29:01 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,25 @@ import LoginFirstPage from "./pages/FirstLoginPage";
 import Header from "./components/Header";
 import SecondLoginPage from "./pages/SecondLoginPage";
 
+const MainPage = () => {
+  return <div>MainPage</div>;
+};
+
 export default function App() {
+  const history = useHistory();
+
+  // Сделал переадресацию, т.к понял что страницы должны иметь пути
+  // вида /login/step-1
+
+  useEffect(() => {
+    history.push("/login/step-1");
+  }, []);
   return (
     <>
       <Header />
       <main className="relative flex flex-col p-4 h-full">
-        <Route path="/" exact component={LoginFirstPage} />
+        <Route path="/" component={MainPage} />
+        <Route path="/login/step-1" component={LoginFirstPage} />
         <Route path="/login/step-2" component={SecondLoginPage} />
       </main>
     </>
